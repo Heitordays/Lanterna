b34ab5ef6656fd1d145a988941065ddcaa78f052
Heitor <heitor.dias.profissional@gmail.com>
2025-05-22 08:11:49 -0300

    na real o bgl la do @dd task tava era zuando a questão, agr ta funfando legal

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Add Task')
 
 @section('content')
-
+{{ $errors }}
 <form method="POST" action="{{ route('task.store') }}">
 @csrf   
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,3 @@
-@dd($task)
 <!DOCTYPE html>
 
 <html lang="pt-BR">
