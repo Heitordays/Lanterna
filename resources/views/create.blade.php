705a1f779e1c44559f5523e2be03e1142b4b6ed0
Heitor <heitor.dias.profissional@gmail.com>
2025-05-22 10:05:28 -0300

    limpando

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,30 +2,50 @@
 
 @section('title', 'Add Task')
 
+@section('styles')
+
 @section('content')
-{{ $errors }}
+
+<head>
+<link rel="stylesheet" href="{{ asset('css/create.css') }}">
+</head>
+
 <form method="POST" action="{{ route('task.store') }}">
 @csrf   
-
+<h1>tralalero</h1>
 <div>
     <label for="title">
 Title
     </label>
     <input type="text" name="title" id="title" />
+    @error('title')
+        <p class="error-message">{{ $message }}</p>
+    @enderror
 </div> 
 
 <div>
     <label for="description">Description</label>
     <textarea name="description" id="description" rows="5"></textarea> 
+    @error('description')
+    <p class="error-message">{{ $message }}</p>
+    @enderror
 </div>
+
 
 <div>
     <label for="long_description">Long Description</label>
     <textarea name="long_description" id="long_description" rows="10"></textarea> 
+    @error('long_description')
+    <p class="error-message">{{ $message }}</p>
+    @enderror
 </div>
+
 
 <div>
 <button type="submit">Add task</button>
+    @error('submit')
+    <p class="error-message">{{ $message }}</p>
+    @enderror
 </div>
 
 </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,45 @@
-<!-- resources/views/layouts/app.blade.php -->
+<!--resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html>
 <head>
     <title>@yield('title')</title>
+    @yield('styles')
 </head>
+
 <body>
     <div class="container">
         @yield('content')
     </div>
+
+    @if (session()->has('success'))
+        <div>
+            {{ session('success') }}
+        </div> 
+        @endif
 </body>
 </html>
+
+
+<!--
+
+o do tutorial é esse:
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Lirili larila</title>
+    </head>
+
+<body>
+    <h1>
+        @yield('title')</h1>
+    </h1>
+    <div>
+        
+        @yield('content')
+    </div>
+
+</body>
+
+</html> -->
