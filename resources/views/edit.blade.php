bfe21e1007ad890797bf23285c5a7b4f9af1a7be
Heitor <heitor.dias.profissional@gmail.com>
2025-05-26 08:43:16 -0300

    finalizado a criação do form e configuração dele, agr ta td funcionando certin

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,50 +1,7 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Task')
-
-@section('styles')
-    <link rel="stylesheet" href="{{ asset('css/create.css') }}">
-@endsection
-
 @section('content')
-    <form method="POST" action="{{ route('tasks.update', $task->id) }}">
-        @csrf
-        @method('PUT')
 
-        <h1>Editar Tarefa</h1>
-
-        <div>
-            <label for="title">Título</label>
-            <input type="text" name="title" id="title" value="{{ old('title', $task->title) }}">
-            @error('title')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <label for="description">Descrição</label>
-            <textarea name="description" id="description">{{ old('description', $task->description) }}</textarea>
-            @error('description')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <label for="long_description">Descrição Longa</label>
-            <textarea name="long_description" id="long_description">{{ old('long_description', $task->long_description) }}</textarea>
-            @error('long_description')
-                <p class="error-message">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div>
-            <button type="submit">Salvar Alterações</button>
-        </div>
-    </form>
-    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('Tem certeza que quer deletar essa task?')">
-    @csrf
-    @method('DELETE')
-    <button type="submit">🗑️ Deletar</button>
-</form>
+@include('form', ['task' => $task ])
 
 @endsection
---
+++ resources/views/form.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('title', isset($task) ? 'Edit Task' : 'Add Task')
+
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/create.css') }}">
+@endsection
+
+@section('content')
+
+<form method="POST" action="{{ isset($task) ? route('tasks.update', $task->id) : route('task.store') }}">
+    @csrf   
+    @isset($task)
+        @method('PUT')
+    @endisset
+
+    <h1>{{ isset($task) ? 'Editar Tarefa' : 'Criar Nova Tarefa' }}</h1>
+
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" value="{{ old('title', $task->title ?? '') }}">
+        @error('title')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+    </div> 
+
+    <div>
+        <label for="description">Description</label>
+        <textarea name="description" id="description" rows="5">{{ old('description', $task->description ?? '') }}</textarea>
+        @error('description')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <div>
+        <label for="long_description">Long Description</label>
+        <textarea name="long_description" id="long_description" rows="7">{{ old('long_description', $task->long_description ?? '') }}</textarea>
+        @error('long_description')
+            <p class="error-message">{{ $message }}</p>
+        @enderror
+    </div>
+
+    <div>
+        <button type="submit">
+            {{ isset($task) ? 'Update Task' : 'Add Task' }}
+        </button>
+    </div>
+</form>
+
+@isset($task)
+    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" onsubmit="return confirm('Tem certeza que quer deletar essa task?')">
+        @csrf
+        @method('DELETE')
+        <button type="submit">🗑️ Deletar</button>
+    </form>
+@endisset
+
+@endsection
